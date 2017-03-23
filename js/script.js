607ae0d31a1394eaf780e1c454d544773ffbe630
Paulo Bordón <bordonbp@gmail.com>
2017-03-23 17:58:39 -0300

    JS IS A MESS
    Probando mover la caja del auto, se logró el movimiento, pero es
    errático

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 //here must be all the javaScript code
 
 console.log('HEY! lets move that car!!'); //no te olvides de cerrar con ;
+
+var gotoA = function() {
+	document.getElementById('move').className = 'maincarinit';
+}
+
+var gotoB = function() {
+	document.getElementById('move').className = 'maincarinit + posb';
+}
+
+var gotoC = function() {
+	document.getElementById('move').className = 'maincarinit + posc';
+}
+
+
+
+var showHide = function() {
+
+	if (document.getElementById('botona').style.display == 'block') {
+
+		document.getElementById('botona').style.display = 'none';
+		document.getElementById('botonb').style.display = 'block';
+
+	} else {
+		document.getElementById('botona').style.display = 'block';
+		document.getElementById('botonb').style.display = 'none';
+	};
+
+	if (document.getElementById('botonc').style.display == 'none') {
+
+		document.getElementById('botonc').style.display = 'block';
+		document.getElementById('botona').style.display = 'none';
+
+	} else {
+		document.getElementById('botonc').style.display = 'none';
+		document.getElementById('botona').style.display = 'block';
+	};
+}
